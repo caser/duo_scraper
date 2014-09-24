40068c5787378f615d875bdb4aeb83b578e14012
Casey Rosengren <nocashvaluedrumz@gmail.com>
2014-09-24 07:25:28 -0600

    Created user seeding and moved it into its own function.

--- duo_scraper.go
+++ duo_scraper.go
@@ -2,13 +2,14 @@ package main
 
 import (
   "fmt"
-  "net/http"
+  // "net/http"
   "io/ioutil"
   "encoding/json"
   // "reflect"
 )
 
 type User struct {
+  Name string
   UserName string
   Languages []LingoData
 }
@@ -24,14 +25,10 @@ func (u *User) save() error {
   return ioutil.WriteFile(filename, []byte("test"), 0600)
 }
 
-func (u *User) Unmarshal(data []byte) {
+func (u *User) Unmarshal(data []byte) error {
   var f interface{}
 
   err := json.Unmarshal(data, &f)
-
-  if err != nil {
-    fmt.Println(err)
-  }
 
   languages := f.(map[string]interface{})["languages"].([]interface{})
 
@@ -44,22 +41,43 @@ func (u *User) Unmarshal(data []byte) {
     ld.Points = int(lingo_map["points"].(float64))
     u.Languages = append(u.Languages, ld)
   }
+
+  return err
+}
+
+func SeedUsers() []User {
+  users := []User{}
+
+  users = append(users,
+    User{ Name: "Kevin K", UserName: "KevinKelle6"},
+    User{ Name: "Max W", UserName: "MaxWofford"},
+    User{ Name: "Casey R", UserName: "nocashvaluedrumz"},
+    User{ Name: "Rob C", UserName: "robcole42"},
+    User{ Name: "Marc G", UserName: "ogoog"},
+    User{ Name: "Wei L", UserName: "puffpuffpuff"},
+    User{ Name: "Matt S", UserName: "Stringerbell1"},
+    User{ Name: "Alexey K", UserName: "alexeymk"},
+    User{ Name: "Nicola H", UserName: "nicolarhughes"},
+    )
+
+  return users
 }
 
 func main() {
-  users := map[string]string
-  users["kevin"] = "KevinKelle6"
-  users["max"] = "MaxWofford"
-  users["casey"] = "nocashvaluedrumz"
-  users["rob"] = "robcole42"
+  users := SeedUsers()
 
-  fmt.Println("In main.")
+  for i := range users {
+    fmt.Println(users[i])
+  }
+
+  /*
+
+    fmt.Println("In main.")
   resp, _ := http.Get("https://www.duolingo.com/users/nocashvaluedrumz")
   defer resp.Body.Close()
 
   data, _ := ioutil.ReadAll(resp.Body)
 
-  u := User{UserName: "rob_cole"}
-
   u.Unmarshal(data)
+  */
 }
