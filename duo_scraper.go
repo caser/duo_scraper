feadff97e593a0e40de62df0a9840f7026e64da9
Casey Rosengren <nocashvaluedrumz@gmail.com>
2014-09-24 11:46:49 -0600

    Saves to and loads user data from a text file.

--- duo_scraper.go
+++ duo_scraper.go
@@ -2,7 +2,7 @@ package main
 
 import (
   "fmt"
-  // "net/http"
+  "net/http"
   "io/ioutil"
   "encoding/json"
   // "reflect"
@@ -20,12 +20,7 @@ type LingoData struct {
   Points int
 }
 
-func (u *User) save() error {
-  filename := "test.txt"
-  return ioutil.WriteFile(filename, []byte("test"), 0600)
-}
-
-func (u *User) Unmarshal(data []byte) error {
+func (u *User) UnmarshalFromDL(data []byte) error {
   var f interface{}
 
   err := json.Unmarshal(data, &f)
@@ -45,10 +40,8 @@ func (u *User) Unmarshal(data []byte) error {
   return err
 }
 
-func SeedUsers() []User {
-  users := []User{}
-
-  users = append(users,
+func seedUsers(users *[]User) {
+  *users = append(*users,
     User{ Name: "Kevin K", UserName: "KevinKelle6"},
     User{ Name: "Max W", UserName: "MaxWofford"},
     User{ Name: "Casey R", UserName: "nocashvaluedrumz"},
@@ -56,28 +49,94 @@ func SeedUsers() []User {
     User{ Name: "Marc G", UserName: "ogoog"},
     User{ Name: "Wei L", UserName: "puffpuffpuff"},
     User{ Name: "Matt S", UserName: "Stringerbell1"},
-    User{ Name: "Alexey K", UserName: "alexeymk"},
+    User{ Name: "Alexey K", UserName: "AlexeyKomi"},
     User{ Name: "Nicola H", UserName: "nicolarhughes"},
+    User{ Name: "Luka K", UserName: "lukakacil"},
+    User{ Name: "Charlie G", UserName: "charlierguo"},
     )
+}
 
-  return users
+func scrapeLanguageData(users *[]User) {
+  for i := range *users {
+    user := (*users)[i]
+    url := "https://www.duolingo.com/users/" + user.UserName
+    
+    resp, err := http.Get(url); if err != nil {
+      fmt.Println(err)
+    }
+    defer resp.Body.Close()
+
+    data, err := ioutil.ReadAll(resp.Body); if err != nil {
+      fmt.Println(err)
+    }
+
+    err = user.UnmarshalFromDL(data); if err != nil {
+      fmt.Println(err)
+    }
+
+    (*users)[i] = user
+  }
+}
+
+func saveUserData(users []User) error {
+
+  filename := "user_data.txt"
+
+  data, err := json.Marshal(users)
+
+  if err != nil {
+    return err
+  }
+
+  return ioutil.WriteFile(filename, data, 0600)
+}
+
+func loadUserData() []byte {
+  filename := "user_data.txt"
+  data, _ := ioutil.ReadFile(filename)
+  return data
+}
+
+func unmarshalSavedData(users *[]User, data []byte) {
+  var f interface{}
+
+  _ = json.Unmarshal(data, &f)
+
+  data_arr := f.([]interface{})
+
+  for i := range data_arr {
+    var u User
+    data_map := data_arr[i].(map[string]interface{})
+    
+    u.Name =  data_map["Name"].(string)
+    u.UserName = data_map["UserName"].(string)
+    
+    language_data := data_map["Languages"].([]interface{})
+    var ld LingoData
+
+    for i := range language_data {
+      lingo_map := language_data[i].(map[string]interface{})
+      ld.Language = lingo_map["Language"].(string)
+      ld.Level = int(lingo_map["Level"].(float64))
+      ld.Points = int(lingo_map["Points"].(float64))
+      u.Languages = append(u.Languages, ld)
+    }
+    *users = append(*users, u)
+  }
 }
 
 func main() {
-  users := SeedUsers()
+  users := []User{}
 
-  for i := range users {
-    fmt.Println(users[i])
-  }
+  data := loadUserData()
 
-  /*
+  unmarshalSavedData(&users, data)
 
-    fmt.Println("In main.")
-  resp, _ := http.Get("https://www.duolingo.com/users/nocashvaluedrumz")
-  defer resp.Body.Close()
+  fmt.Println(users[4])
 
-  data, _ := ioutil.ReadAll(resp.Body)
+  // seedUsers(&users)
 
-  u.Unmarshal(data)
-  */
+  // scrapeLanguageData(&users)
+
+  // saveUserData(users)
 }
